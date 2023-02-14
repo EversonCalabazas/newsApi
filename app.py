5e93ee1ef3d2e9e2d0da43170d15436f77f97aa3
EversonCalabazas <55508854+EversonCalabazas@users.noreply.github.com>
2023-02-14 18:12:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import requests, json, datetime, pandas as pd
-from IPython.display import HTML
 
 today = datetime.datetime.today().date()
 today = today.strftime('%Y-%m-%d') 
-
 
 url = ('https://newsapi.org/v2/everything?'
        'q=Apple&'
@@ -21,31 +19,3 @@ date = data.publishedAt
 newsDF = pd.concat([date,content], axis=1)
 newsDF.index=[''] * len(newsDF)
 print(newsDF)
-
-newsDF = newsDF.to_html()
-
-
-from email.policy import SMTP
-import smtplib
-import email.message
-
-corpo_email = f"""
-<p>Olá Everson</p>
-<p>Email automático</p>
-"{newsDF}"
-"""
-
-msg = email.message.Message()
-msg['Subject'] = "Assunto"
-msg['From'] = "everson.esc@gmail.com"
-msg['To'] = "everson.esc@gmail.com"
-password = 'ezhhrsdurruzrvlo'
-msg.add_header('Content-Type','text/html')
-msg.set_payload(corpo_email)
-
-s = smtplib.SMTP('smtp.gmail.com:587')
-s.starttls()
-
-s.login(msg['From'], password)
-s.sendmail(msg['From'],[msg['To']], msg.as_string().encode('utf-8'))
-print('Email enviado')
